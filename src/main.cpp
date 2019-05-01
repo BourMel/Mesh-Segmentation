11c335ceaa9e0bd1c2b2b363685674044446c6bf
nathan <nathan.roth67@yahoo.fr>
2019-05-01 17:05:48 +0200

    add test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "mesh.hpp"
+#include "edge.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -9,6 +10,25 @@ int main(int argc, char const *argv[])
     if(argc == 2)
     {
         mesh.importOFF(argv[1]);
+        mesh.dissolveEdge(mesh.edges()[4]);
+        //mesh.dissolveEdge(mesh.edges()[0]);
+        //mesh.skeletonization();
+        for(auto v: mesh.vertices())
+        {
+            std::cout << v->id() << ": (" << v->pos().x << ", " << v->pos().y << ", " << v->pos().z << ")" << std::endl;
+        }
+        for(auto e: mesh.edges())
+        {
+            std::cout << e->v1()->id() << " " << e->v2()->id() << std::endl;
+        }
+        for(auto f: mesh.faces())
+        {
+            for(auto e: f->edges())
+            {
+                std::cout << e->v1()->id() << " " << e->v2()->id() << " | ";
+            }
+            std::cout << std::endl;
+        }
     }
     
     return 0;
