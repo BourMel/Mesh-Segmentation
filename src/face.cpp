3289382eb8b223fcb5a2e8846adb481033ffed00
nathan <nathan.roth67@yahoo.fr>
2019-05-03 22:20:41 +0200

    formating

--- src/face.cpp
+++ src/face.cpp
@@ -15,22 +15,25 @@ void Face::addEdge(Edge *e)
     m_edges.push_back(e);
 }
 
-bool Face::isSimple() {
+bool Face::isSimple()
+{
     bool isSimple = true;
 
     // test each couple of edges
-    for(unsigned int i=0; (i<m_edges.size()-1) && isSimple; i++) {
-      for(unsigned int j=i+1; (j<m_edges.size()) && isSimple; j++) {
-
-        if(m_edges[i]->intersectWith(m_edges[j])) {
-          isSimple = false;
+    for (unsigned int i = 0; (i < m_edges.size() - 1) && isSimple; i++)
+    {
+        for (unsigned int j = i + 1; (j < m_edges.size()) && isSimple; j++)
+        {
+            if (m_edges[i]->intersectWith(m_edges[j]))
+            {
+                isSimple = false;
+            }
         }
-      }
     }
     return isSimple;
 }
 
-std::vector<Edge*> &Face::edges()
+std::vector<Edge *> &Face::edges()
 {
     return m_edges;
 }
@@ -44,7 +47,7 @@ std::ostream &operator<<(std::ostream &o, Face &f)
 {
     std::stringstream ss;
     ss << f.id() << " ";
-    for(auto e : f.edges())
+    for (auto e : f.edges())
     {
         ss << *e;
     }
