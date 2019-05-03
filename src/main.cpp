5e8fe87d0d2d0f6d14d61145e21449523f200f2a
nathan <nathan.roth67@yahoo.fr>
2019-05-03 14:54:08 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,9 @@ int main(int argc, char const *argv[])
     if(argc == 2)
     {
         mesh.importOFF(argv[1]);
-        mesh.dissolveEdge(mesh.edges()[4]);
+        //mesh.dissolveEdge(mesh.edges()[4]);
         //mesh.dissolveEdge(mesh.edges()[0]);
-        //mesh.skeletonization();
-        for(auto v: mesh.vertices())
-        {
-            std::cout << v->id() << ": (" << v->pos().x << ", " << v->pos().y << ", " << v->pos().z << ")" << std::endl;
-        }
-        for(auto e: mesh.edges())
-        {
-            std::cout << e->v1()->id() << " " << e->v2()->id() << std::endl;
-        }
-        for(auto f: mesh.faces())
-        {
-            for(auto e: f->edges())
-            {
-                std::cout << e->v1()->id() << " " << e->v2()->id() << " | ";
-            }
-            std::cout << std::endl;
-        }
+        mesh.skeletonization();
     }
     
     return 0;
